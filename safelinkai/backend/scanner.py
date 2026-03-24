a2a768fea8bde33409c4de4d7c622a0634a05b93
safelink-ai-project <agpeter09@gmail.com>
2026-03-24 14:41:45 +0100

    Update scanner.py

--- safelinkai/backend/scanner.py
+++ safelinkai/backend/scanner.py
@@ -4,32 +4,20 @@ from behaviour_analysis import behaviour_analysis
 from risk_engine import calculate_risk
 
 def scan_url(url):
-
     heuristic_score, heuristic_flags = url_heuristics(url)
-
     scan_data = dynamic_scan(url)
-
     threats = behaviour_analysis(scan_data)
-
     ai_result = 0
-
     risk_score, status = calculate_risk(
         ai_result,
         heuristic_score,
         threats
     )
-
     return {
-
         "url": url,
-
         "risk_score": risk_score,
-
         "status": status,
-
         "heuristic_flags": heuristic_flags,
-
         "behaviour_flags": threats,
-
         "page_info": scan_data
     }
